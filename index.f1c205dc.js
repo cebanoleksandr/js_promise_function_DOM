3a1fd910a888298ec56b7dc7a0e8b8fdc843fd15
alex <cebanoleksandr@gmail.com>
2023-11-28 09:34:32 +0200

    make build

---
+++ index.f1c205dc.js
@@ -0,0 +1,3 @@
+function e(e,n){return new Promise(function(t){e.addEventListener(n,function(){t("It was ".concat(n," on the element:\n        ").concat(e.nodeName,", id: ").concat(e.id,"."))})})}var n=function(e){var n=document.createElement("div");n.className="message",n.textContent=e,document.body.append(n)},t=document.getElementById("login"),c=document.getElementById("password"),o=document.getElementById("submit");e(t,"click").then(n),e(c,"click").then(n),e(o,"click").then(n),e(t,"input").then(n),e(c,"input").then(n),e(t,"blur").then(n),e(c,"blur").then(n),e(o,"blur").then(n);//# sourceMappingURL=index.f1c205dc.js.map
+
+//# sourceMappingURL=index.f1c205dc.js.map
